cc8b6cfa962fc45eec958a51dc0162dfdfa9e01e
IdeoG <ideogaib@gmail.com>
2018-07-30 10:59:23 +0300

    Добавил скрипты в свой проект. Из последнего сделал текстовое отображение действия эффекта ускорения.

---
+++ Assets/StudentAssets/Scripts/EnemyTankController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class EnemyTankController : MonoBehaviour
+{
+    [SerializeField] private float _maxHealth;
+    [SerializeField] private LayerMask _targetMask;
+    
+    [SerializeField] private float _viewRadius;
+    [Range(0, 360)] [SerializeField] private float _viewAngle;
+
+    [SerializeField] private EnemyHealthBarController _healthBarController;
+    
+    private bool _isFindEnemiesActive = true;
+    private float _health;
+
+    public void Hit(float hit)
+    {
+        _health -= hit;
+        _health = _health > 0f ? _health : 0f;
+        
+        _healthBarController.SetHealth(_health, _maxHealth);
+        
+        Debug.Log(string.Format("Hit: _health = {0}", _health));
+        
+        if (Math.Abs(_health) < 1f)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    private void Start()
+    {
+        StartCoroutine(FindEnemiesWithDelay(0.2f));
+    }
+    
+    private void OnEnable()
+    {
+        _health = _maxHealth;
+    }
+    
+    private IEnumerator FindEnemiesWithDelay(float delay)
+    {
+        while (_isFindEnemiesActive)
+        {
+            yield return new WaitForSeconds(delay);
+            FindVisibleEnemies();
+        }
+    }
+
+    private void FindVisibleEnemies()
+    {
+        var targetsInViewRadius = Physics.OverlapSphere(transform.position, _viewRadius, _targetMask);
+
+        foreach (var _collider in targetsInViewRadius)
+        {    
+            var target = _collider.transform;
+            var dirToTarget = (target.position - transform.position).normalized;
+            if (Vector3.Angle(transform.forward, dirToTarget) < _viewAngle / 2)
+            {
+//                if (_collider.name.Equals("EnemyTank"))
+//                {
+//                    Debug.DrawLine(transform.position, target.position, Color.red);
+//                    _healthBarController.ShowHealthBar();
+//                }
+//                else
+//                {
+//                    _healthBarController.HideHealthBar();
+//                }
+                
+            }
+        }
+        
+        
+    }
+}
---
+++ Assets/StudentAssets/Scripts/Shell.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Shell : MonoBehaviour {
+
+	private void Start () {
+		Destroy(gameObject, 1.5f);
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		var name = other.collider.name;
+		var tag = other.collider.tag;
+
+		if (tag.Equals("Enemy"))
+		{
+			if (name.Equals("EnemyTank"))
+			{
+				Debug.Log("Shell hitted EnemyTank.");
+				var enemy = other.collider.gameObject.GetComponent<EnemyTankController>();
+				enemy.Hit(Random.Range(20f, 30f)); 
+			}
+		}
+	}
+}
